d63338e980cef23d80030a41ada71df014b5d791
Elyas Santana <picpay@AGM-609865.local>
2022-08-23 16:48:37 -0300

    Initial commit

---
+++ app/exercicio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/exercicio.css">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+
+<body class="exercicio">
+  <header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Visualização dos Exercícios</h2>
+  </header>
+  <nav class="navegacao">
+    <a href="#" class="verde">Sem Formatação</a>
+    <a href="index.php" class="vermelho">Voltar</a>
+  </nav>
+  <main class="principal">
+    <div class="conteudo">
+
+    </div>
+  </main>
+  <footer class="rodape">
+    COD3R & ALUNOS ⓒ <? date('Y'); ?>
+  </footer>
+</body>
+
+</html>
