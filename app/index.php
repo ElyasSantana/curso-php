315094e1755420dc69c89abaa1549b07f691f9df
Elyas Santana <picpay@AGM-609865.local>
2022-08-24 17:06:13 -0300

    Básico do php, html e css

--- app/index.php
+++ app/index.php
@@ -5,8 +5,7 @@
   <meta charset="UTF-8">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="assets/css/styles.css">
 
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,11 +22,33 @@
     <div class="conteudo">
       <nav class="modulos">
         <div class="modulo verde">
-          <h3>Módulo 01 - Básico</h3>
+          <h3>Básico</h3>
           <ul>
-            <li><a href="exercicio.php">Exercício 1</a></li>
-            <li><a href="#">Exercício 2</a></li>
-            <li><a href="#">Exercício 3</a></li>
+            <li>
+              <a href="exercicio.php?dir=basico&file=ola">
+                Olá PHP
+              </a>
+            </li>
+            <li>
+              <a href="exercicio.php?dir=basico&file=html">
+                Integração HTML
+              </a>
+            </li>
+            <li>
+              <a href="exercicio.php?dir=basico&file=css">
+                Integração CSS
+              </a>
+            </li>
+            <li>
+              <a href="exercicio.php?dir=basico&file=comentarios">
+                Comentários PHP
+              </a>
+            </li>
+            <li>
+              <a href="exercicio.php?dir=basico&file=desafio">
+                Desafio
+              </a>
+            </li>
           </ul>
         </div>
         <div class="modulo vermelho">
